3d502bee5559c1856f50b6f37bd0375d46b8cc76
Robin Lilja <robin.lilja@gmail.com>
2015-09-26 13:21:05 +0200

    Create sma.cpp

---
+++ sma.cpp
@@ -0,0 +1,12 @@
+/**
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <robin.lilja@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. - Robin Lilja
+ *
+ * @file sma.cpp
+ * @author Robin Lilja
+ * @date 26 Sep 2015
+ */
+
+#include "sma.h"
